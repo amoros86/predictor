ba89bcb3c12704f6dc55c53d7ae1498291513b75
amoros86 <amoros86@gmail.com>
2018-10-16 20:52:24 +0200

    subo proyecto

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DashboardController extends AbstractController{
+
+    public function index() {
+        $text= "ESTO SERA EL INDICE";
+
+        
+        return $this->render('Dashboard/index.html.twig', [
+            'text' => $text,
+        ]);
+    }
+
+}
+?>
